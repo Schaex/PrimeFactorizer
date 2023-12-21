9caed7a978e2b4c31d5f80cb36df0b6460401ab5
schaex <schaex@gmail.com>
2023-12-21 01:51:31 +0100

    Cleaned the UI and added comments

--- src/ApplicationConstants.java
+++ src/ApplicationConstants.java
@@ -2,5 +2,5 @@ public interface ApplicationConstants {
     /**
      * Utility interface to prevent duplicate code.
      */
-    static final String linesep = System.getProperty("line.separator");
+    String linesep = System.getProperty("line.separator");
 }
